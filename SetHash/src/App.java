f0db7fa43e4129fcfaa57c1b4cd79ecf298a8856
Patrick Liu <liujpatrick@gmail.com>
2024-09-17 09:15:29 -0700

    SetHash HW completed

---
+++ SetHash/src/App.java
@@ -0,0 +1,153 @@
+// Sets and Hash tables(Also called dictionaries or just hashes) are a way of storing information that stores not by index but by hash function. 
+
+// Please create a java or python script that solves the following problems.
+
+// Create a function called mostFrequent that takes in an array and returns the element within the array that occurs the most often
+// Ex. mostFrequent([ 40,50,30,40,50,30,30]) -> 30
+// You can assume one value will be more frequent and do not need to account for ties.
+// Create a function that is called duplicates that takes in an array and returns true if the array contains any duplicate values
+// Ex. duplicates([1, 2, 3, 1]) -> True
+// Ex. duplicates([1, 2, 3, 4]) -> False
+// Create a function called subSet that takes in two arrays and checks if the second array is a subset of the first array. That is to say all values within the second array appear within the first array.
+// Ex. subSet([11, 1, 13, 21, 3, 7], [11, 3, 7, 1]) - > True
+// Ex. subSet([10, 5, 2, 23, 19], [19, 5, 3]) - > False
+// Both arrays are unsorted and contains no duplicates within themselves.
+// Create a function called frequencySort that takes in an array and returns an array sorted such that the most frequent elements are at the front and least frequent are at the back. If two elements share a frequency maintain their order from the original array.
+// frequencySort([2, 5, 2, 8, 5, 6, 8, 8]) -> [8, 8, 8, 2, 2, 5, 5, 6] 
+// Note that 2 and 5 have the same frequency but because 2 comes first in the original list it is first in the returned list.
+// Create a function called twoSum that takes in an array and a target value. Return the indices of the two numbers within the array that add up to the target value
+// Ex. twoSum([2, 7, 11, 15], 9) -> [0,1]
+// Ex. twoSum([3, 3, 5, 2], 6 -> [0,1]
+// You can assume each array has exactly one solution.
+// The given array should be assumed to be out of order.
+
+import java.util.*;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        App app = new App();
+        
+        // Test mostFrequent
+        int[] arr1 = {40, 50, 30, 40, 50, 30, 30};
+        System.out.println(app.mostFrequent(arr1));
+
+        // Test duplicates
+        int[] arr2 = {1, 2, 3, 1};
+        System.out.println(app.duplicates(arr2));
+
+        int[] arr3 = {1, 2, 3, 4};
+        System.out.println(app.duplicates(arr3));
+
+        // Test subSet
+        int[] set1 = {11, 1, 13, 21, 3, 7};
+        int[] set2 = {11, 3, 7, 1};
+        System.out.println(app.subSet(set1, set2));
+
+        int[] set3 = {10, 5, 2, 23, 19};
+        int[] set4 = {19, 5, 3};
+        System.out.println(app.subSet(set3, set4));
+
+        // Test frequencySort
+        int[] arr4 = {2, 5, 2, 8, 5, 6, 8, 8};
+        System.out.println(Arrays.toString(app.frequencySort(arr4)));
+
+        // Test twoSum
+        int[] arr5 = {2, 7, 11, 15};
+        int target1 = 9;
+        System.out.println(Arrays.toString(app.twoSum(arr5, target1)));
+
+        int[] arr6 = {3, 3, 5, 2};
+        int target2 = 6;
+        System.out.println(Arrays.toString(app.twoSum(arr6, target2)));
+    }
+    
+    public int mostFrequent(int[] arr) throws Exception {
+
+        HashMap<Integer, Integer> hashTable = new HashMap<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            hashTable.put(arr[i], 1 + hashTable.getOrDefault(arr[i], 0));
+        }
+
+        int mostFrequent = -1;
+        int count = 0;
+        for (int num : hashTable.keySet()){
+            if (hashTable.get(num) > count){
+                count = hashTable.get(num);
+                mostFrequent = num;
+            }
+        }
+
+        return mostFrequent;
+    }
+
+    public boolean duplicates(int[] arr) throws Exception {
+
+        Set<Integer> set = new HashSet<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            set.add(arr[i]);
+        }
+
+        return arr.length != set.size();
+    }
+
+    public boolean subSet(int[] arr1, int[] arr2) throws Exception {
+
+        HashMap<Integer, Integer> hashTable = new HashMap<>();
+
+        for (int i = 0; i < arr1.length; i++) {
+            hashTable.put(arr1[i], 1 + hashTable.getOrDefault(arr1[i], 0));
+        }
+
+        for (int i = 0; i < arr2.length; i++){
+            if (hashTable.get(arr2[i]) == null){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public int[] frequencySort(int[] arr) throws Exception {
+
+        HashMap<Integer, Integer> hashTable = new HashMap<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            hashTable.put(arr[i], 1 + hashTable.getOrDefault(arr[i], 0));
+        }
+
+        List<Integer> returnArray = new ArrayList<Integer>();
+        for (int num : arr){
+            returnArray.add(num);
+        }
+
+        returnArray.sort((a, b) -> {
+            int freqA = hashTable.get(a);
+            int freqB = hashTable.get(b);
+    
+            if (freqA != freqB) {
+                return Integer.compare(freqB, freqA);
+            }
+    
+            return Integer.compare(a, b);
+        });
+    
+        return returnArray.stream().mapToInt(i -> i).toArray();
+    }
+
+    public int[] twoSum(int[] arr, int target) throws Exception {
+
+        HashMap<Integer, Integer> hashTable = new HashMap<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            int sum = target - arr[i];
+
+            if (hashTable.containsKey(sum)) {
+                return new int[]{hashTable.get(sum), i};
+            }
+            hashTable.put(arr[i], i);
+            
+        }
+        throw new IllegalArgumentException("No");
+    }
+}
--- SortSearch/src/SearchSort.java
+++ SortSearch/src/SearchSort.java
@@ -18,13 +18,34 @@ import java.util.Arrays;
 
 class SearchSort {
     public static void main(String[] args) {
-        int[] arr = {33, 3, 3, 3, 4, -1, 4,2,4,6,3,2};
-        // BubbleSort(arr);
-        MergeSort(arr);
-        System.out.println(Arrays.toString(arr));
+        int[] arr = {33, 3, 3, 3, 4, -1, 4, 2, 4, 6, 3, 2};
+
+        // Test BubbleSort
+        System.out.println("BubbleSort: " + Arrays.toString(BubbleSort(arr.clone())));
+
+        // Test MergeSort
+        int[] mergeSortedArr = arr.clone();
+        MergeSort(mergeSortedArr);
+        System.out.println("MergeSort: " + Arrays.toString(mergeSortedArr));
+
+        // Test findInsertIndex
+        int[] sortedArr = {1, 3, 5, 6};
+        System.out.println("InsertIndex: " + findInsertIndex(sortedArr, 5));
+
+        System.out.println("InsertIndex: " + findInsertIndex(sortedArr, 2));
+
+        // Test generatePascalsTriangle
+        System.out.println("PascalTriangle: " + Arrays.deepToString(generatePascalsTriangle(5)));
+
+        // Test findFirstAndLastIndex
+        int[] sortedArrWithDuplicates = {1, 2, 2, 2, 3, 4, 5};
+        System.out.println("FirstAndLastIndex: " + Arrays.toString(findFirstAndLastIndex(sortedArrWithDuplicates, 2)));
+        System.out.println("FirstAndLastIndex: " + Arrays.toString(findFirstAndLastIndex(sortedArrWithDuplicates, 6)));
 
     }
 
+    // Question 1: Bubble Sort
+    // Best: O(n), Avg/Worst: O(n^2)
     public static int[] BubbleSort(int[] arr) {
         boolean notSorted;
         do {
@@ -41,21 +62,108 @@ class SearchSort {
         return arr;
     }
 
-    public static int[] MergeSort(int[] arr) {
-        boolean notSorted;
-        do {
-            notSorted = false;
-            for (int i = 0; i < arr.length - 1; i++) {
-                if (arr[i] > arr[i + 1]) {
-                    int temp = arr[i + 1];
-                    arr[i + 1] = arr[i];
-                    arr[i] = temp;
-                    notSorted = true;
-                }
+    // Question 2: Merge Sort
+    // Best/Avg/Worst: O(n log n)
+    public static void MergeSort(int[] arr) {
+        if (arr.length > 1) {
+            int mid = arr.length / 2;
+            int[] left = Arrays.copyOfRange(arr, 0, mid);
+            int[] right = Arrays.copyOfRange(arr, mid, arr.length);
+
+            MergeSort(left);
+            MergeSort(right);
+
+            merge(arr, left, right);
+        }
+    }
+
+    private static void merge(int[] arr, int[] left, int[] right) {
+        int i = 0;
+        int j = 0;
+        int k = 0;
+        
+        while (i < left.length && j < right.length) {
+            if (left[i] <= right[j]) {
+                arr[k++] = left[i++];
+            } else {
+                arr[k++] = right[j++];
             }
-        } while (notSorted);
-        return arr;
+        }
+        while (i < left.length) {
+            arr[k++] = left[i++];
+        }
+        while (j < right.length) {
+            arr[k++] = right[j++];
+        }
+    }
+
+    // Question 3: Find Insert Index
+    // Best: O(1), Avg/Worst: O(log n)
+    public static int findInsertIndex(int[] arr, int target) {
+        int left = 0, right = arr.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] == target) {
+                return mid;
+            } else if (arr[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return left;
+    }
+
+    // Question 4: Pascal's Triangle
+    // Best/Avg/Worst: O(n^2)
+    public static int[][] generatePascalsTriangle(int rows) {
+        int[][] triangle = new int[rows][];
+
+        for (int i = 0; i < rows; i++) {
+            triangle[i] = new int[i + 1];
+            triangle[i][0] = 1; 
+            triangle[i][i] = 1; 
+
+            for (int j = 1; j < i; j++) {
+                triangle[i][j] = triangle[i - 1][j - 1] + triangle[i - 1][j];
+            }
+        }
+        return triangle;
+    }
+
+    // Question 5: Find First and Last Index
+    // Best: O(1), Avg/Worst: O(log n)
+    public static int[] findFirstAndLastIndex(int[] arr, int target) {
+        int[] result = {-1, -1};
+        int left = 0, right = arr.length - 1;
+    
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] == target) {
+                result[0] = mid; // maybe first index
+                right = mid - 1; // Keep looking left
+            } else if (arr[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+    
+        left = 0;
+        right = arr.length - 1;
+    
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] == target) {
+                result[1] = mid; // maybe last index
+                left = mid + 1; // Keep going right
+            } else if (arr[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+    
+        return result;
     }
 }
-
-// SearchSort x = new SearchSort();
